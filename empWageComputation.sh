015e4119800e7a115c3716cc3588978ee1464dbe
Sree Pittala <ssapittala1@sheffield.ac.uk>
2020-07-16 16:22:22 +0100

    UC-6 Completed: Total hrs and wage calculated for 1 month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,19 +2,18 @@
 randomNum=$((RANDOM%3))
 isFullTime=1
 isPartTime=2
-wagePerHr=20
 workingDays=20
 case $randomNum in
 $isFullTime)
-	empHr=8
+	totalHrs=100
 ;;
 $isPartTime)
-	empHr=4
+	totalHrs=60
 ;;
 *)
-	empHr=0
+	totalHrs=0
 	echo "Employee is absent, so cannot calculate wage"
 ;;
 esac
-salary=$(($empHr*$wagePerHr*$workingDays))
+salary=$(($totalHrs*$workingDays))
 echo "Employee Salary: $salary"
