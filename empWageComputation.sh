d78f62cfc50f2f6c6ce9bb8cbe81fe9afe96a332
Sree Pittala <ssapittala1@sheffield.ac.uk>
2020-07-16 14:47:12 +0100

    UC-3 Completed: Part-time employee wages also calculated

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,20 @@
 #!/bin/bash -x
-randomNum=$((RANDOM%2))
+randomNum=$((RANDOM%3))
 isFullTime=1
+isPartTime=2
 wagePerHr=20
 fullDayHr=8
+partTimeHr=4
 if [ $randomNum -eq 1 ] && [ isFullTime ]
 then
 	salary=$((wagePerHr*fullDayHr))
-	echo "Salary per day: $salary"
+	echo "Salary per day for a full-time employee: $salary"
+
+elif [ $randomNum -eq 2 ] && [ isPartTime ]
+then
+   salary=$((wagePerHr*partTimeHr))
+   echo "Salary per day for a part-time employee: $salary"
+
 else
-	echo "Employee is absent, so cannott calculate wages"
+	echo "Employee is absent, so cannot calculate wages"
 fi
