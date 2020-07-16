d533375fa3746aba44329e290ab39ea2cf598f55
Sree Pittala <ssapittala1@sheffield.ac.uk>
2020-07-16 20:23:14 +0100

    UC-8 Completed: Stored the daily wage along with total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,22 +3,40 @@ randomNum=$((RANDOM%3))
 isFullTime=1
 isPartTime=2
 workingDays=20
+wagePerHr=20
+fullTimeHours=8
+partTimeHours=4
 
 function calculateSalaryForFullMonth() {
    salary=$(($totalHrs*$workingDays));
 }
 
+function calculateSalaryForOneDayFullTime() {
+	salary=$(($fullTimeHours*$wagePerHr))
+}
+
+function calculateSalaryForOneDayPartTime() {
+   salary=$(($partTimeHours*$wagePerHr))
+}
 
 case $randomNum in
 $isFullTime)
 	totalHrs=100
+
 	calculateSalaryForFullMonth
-	echo "Salary for full-time: $salary"
+	echo "Salary for 1 month (full-time): $salary"
+
+   calculateSalaryForOneDayFullTime
+	echo "Salary for 1 day (full-time): $salary"
 ;;
 $isPartTime)
 	totalHrs=60
+
 	calculateSalaryForFullMonth
-	echo "Salary for part-time: $salary"
+	echo "Salary for 1 month (part-time): $salary"
+
+	calculateSalaryForOneDayPartTime
+	echo "Salary for 1 day (part-time): $salary"
 ;;
 *)
 	totalHrs=0
