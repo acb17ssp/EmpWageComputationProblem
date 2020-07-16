5a22188494b6a250099036ab977992172035e90b
Sree Pittala <ssapittala1@sheffield.ac.uk>
2020-07-16 15:20:42 +0100

    UC-4 Completed: Switch case statements added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,18 +3,18 @@ randomNum=$((RANDOM%3))
 isFullTime=1
 isPartTime=2
 wagePerHr=20
-fullDayHr=8
-partTimeHr=4
-if [ $randomNum -eq 1 ] && [ isFullTime ]
-then
-	salary=$((wagePerHr*fullDayHr))
-	echo "Salary per day for a full-time employee: $salary"
 
-elif [ $randomNum -eq 2 ] && [ isPartTime ]
-then
-   salary=$((wagePerHr*partTimeHr))
-   echo "Salary per day for a part-time employee: $salary"
-
-else
-	echo "Employee is absent, so cannot calculate wages"
-fi
+case $randomNum in
+$isFullTime)
+	empHr=8
+;;
+$isPartTime)
+	empHr=4
+;;
+*)
+	empHr=0
+	echo "Employee is absent, so cannot calculate wage"
+;;
+esac
+salary=$(($empHr*$wagePerHr))
+echo "Employee Salary: $salary"
