d17bdcdc3f3e5f11b40967364f6ce1a4de40f514
Sree Pittala <ssapittala1@sheffield.ac.uk>
2020-07-16 20:12:13 +0100

    UC-7 Completed: Total salary for 1 month calculated through function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,17 +3,25 @@ randomNum=$((RANDOM%3))
 isFullTime=1
 isPartTime=2
 workingDays=20
+
+function calculateSalaryForFullMonth() {
+   salary=$(($totalHrs*$workingDays));
+}
+
+
 case $randomNum in
 $isFullTime)
 	totalHrs=100
+	calculateSalaryForFullMonth
+	echo "Salary for full-time: $salary"
 ;;
 $isPartTime)
 	totalHrs=60
+	calculateSalaryForFullMonth
+	echo "Salary for part-time: $salary"
 ;;
 *)
 	totalHrs=0
 	echo "Employee is absent, so cannot calculate wage"
 ;;
 esac
-salary=$(($totalHrs*$workingDays))
-echo "Employee Salary: $salary"
